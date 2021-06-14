f99b5615f32d136668474cc20d1ac73918062c7a
pradeepshankar <pradeep.shankaraiah@itpeoplecorp.com>
2021-06-14 15:19:26 +0530

    Login component

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose
-    .connect('mongodb+srv://prdpifs:Pradmongo00@mysite.92k5n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("db connected succesfully");
-    })
-        .catch(err => {
-        console.log("Error connecting to DB", err);
-    });
+.connect('mongodb+srv://prdpifs:Pradmongo00@mysite.92k5n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("db connected succesfully");
+})
+.catch(err => {
+    console.log("Error connecting to DB", err);
+});
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const port = 4040;
 app.use(express.json());
 app.listen(port, () => {
